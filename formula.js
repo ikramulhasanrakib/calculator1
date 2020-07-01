fa69ca25bf3af839590ae91b493c1786fc811951
ikramulhasanrakib <67625109+ikramulhasanrakib@users.noreply.github.com>
2020-07-01 22:11:33 +0600

    Add files via upload

---
+++ formula.js
@@ -0,0 +1,23 @@
+
+alert("Hi!This is a simple Calculator.")
+alert("Don't forget to thank me!")
+
+
+function insert(val){
+  document.getElementById("output").value+=val;
+}
+function equal(){
+  let x = document.getElementById("output").value;
+  let y = eval(x); 
+  document.getElementById("output").value=y; 
+}
+function clr(){
+  document.getElementById("output").value="";
+}
+function backspace(){
+    var prevalue=document.getElementById("output").value;
+    document.getElementById("output").value=prevalue.substr(0,prevalue.length-1);
+}
+function ac(){
+document.getElementById("output").value="0";
+}
